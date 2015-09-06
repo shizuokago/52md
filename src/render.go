3802bcea1c10fdf8cc10ebd80bb7a32bb704c058
secondarykey <secondarykey@gmail.com>
2015-09-06 15:53:01 +0900

    reactoring

--- src/render.go
+++ src/render.go
@@ -1,9 +1,16 @@
 package go2md
 
 import (
+	"bytes"
+	"fmt"
+	"golang.org/x/tools/godoc/static"
 	"golang.org/x/tools/present"
 	"html/template"
+	"io/ioutil"
+	"mime"
+	"net/http"
 	"path/filepath"
+	"time"
 )
 
 type Who struct {
@@ -16,6 +23,23 @@ func (s Who) AttributeFile(name string) ([]byte, error) {
 	return []byte(s.data), nil
 }
 
+var scripts = []string{"jquery.js", "jquery-ui.js", "playground.js", "play.js"}
+var modTime = time.Now()
+var scriptByte []byte
+
+func init() {
+	playScript("./", "HTTPTransport")
+	present.PlayEnabled = true
+	// App Engine has no /etc/mime.types
+	mime.AddExtensionType(".svg", "image/svg+xml")
+	http.HandleFunc("/play.js", playHandler)
+}
+
+func playHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/javascript")
+	http.ServeContent(w, r, "", modTime, bytes.NewReader(scriptByte))
+}
+
 func createTemplate() (*template.Template, error) {
 	base := "./"
 	actionTmpl := filepath.Join(base, "templates/action.tmpl")
@@ -26,4 +50,25 @@ func createTemplate() (*template.Template, error) {
 		return nil, err
 	}
 	return tmpl, nil
+}
+
+func playable(c present.Code) bool {
+	return present.PlayEnabled && c.Play && c.Ext == ".go"
+}
+
+func playScript(root, transport string) {
+	var buf bytes.Buffer
+	for _, p := range scripts {
+		if s, ok := static.Files[p]; ok {
+			buf.WriteString(s)
+			continue
+		}
+		b, err := ioutil.ReadFile(filepath.Join(root, "./static", p))
+		if err != nil {
+			panic(err)
+		}
+		buf.Write(b)
+	}
+	fmt.Fprintf(&buf, "\ninitPlayground(new %v());\n", transport)
+	scriptByte = buf.Bytes()
 }
--- src/slide.go
+++ src/slide.go
@@ -1,41 +1,19 @@
 package go2md
 
 import (
-	"bytes"
-	"fmt"
-	"golang.org/x/tools/godoc/static"
 	_ "golang.org/x/tools/playground"
 	"golang.org/x/tools/present"
 	"html/template"
-	"io"
-	"io/ioutil"
-	"mime"
 	"net/http"
-	"os"
-	"path/filepath"
 	"strings"
-	"time"
 
 	"google.golang.org/appengine"
 	"google.golang.org/appengine/datastore"
-	"google.golang.org/appengine/log"
 
 	"github.com/pborman/uuid"
 )
 
-var (
-	contentTemplate map[string]*template.Template
-)
-var scripts = []string{"jquery.js", "jquery-ui.js", "playground.js", "play.js"}
-
 func init() {
-	basePath := "./"
-	initTemplates(basePath)
-	playScript(basePath, "HTTPTransport")
-	present.PlayEnabled = true
-	// App Engine has no /etc/mime.types
-	mime.AddExtensionType(".svg", "image/svg+xml")
-	http.HandleFunc("/play.js", playHandler)
 
 	http.HandleFunc("/me/slide/create", createHandler)
 	http.HandleFunc("/me/slide/edit/", editHandler)
@@ -78,6 +56,7 @@ func putSlide(r *http.Request, key string) (*Slide, error) {
 }
 
 func getSlide(r *http.Request, key string) (*Slide, error) {
+
 	c := appengine.NewContext(r)
 	k := datastore.NewKey(c, "Slide", key, 0, nil)
 	rtn := Slide{}
@@ -92,7 +71,7 @@ func getSlide(r *http.Request, key string) (*Slide, error) {
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
-	//c := appengine.NewContext(r)
+
 	urls := strings.Split(r.URL.Path, "/")
 	keyId := urls[4]
 
@@ -116,10 +95,10 @@ type SlideView struct {
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 
-	u, _ := getUser(r)
-
 	urls := strings.Split(r.URL.Path, "/")
 	keyId := urls[4]
+
+	u, _ := getUser(r)
 	s, _ := getSlide(r, keyId)
 
 	data := Who{
@@ -139,9 +118,6 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	slideTxt += "@" + u.TwitterId + "\n"
 	slideTxt += "\n"
 	slideTxt += s.Markdown
-
-	c := appengine.NewContext(r)
-	log.Infof(c, slideTxt)
 
 	//52md
 	//Golang Present Tools Editor
@@ -179,96 +155,4 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	//doc.Render(w, tmpl)
-}
-
-func playable(c present.Code) bool {
-	return present.PlayEnabled && c.Play && c.Ext == ".go"
-}
-
-func slideHandler(w http.ResponseWriter, r *http.Request) {
-	const base = "./slides"
-	name := filepath.Join(base, r.URL.Path)
-	if isDoc(name) {
-		err := renderDoc(w, name)
-		if err != nil {
-			http.Error(w, err.Error(), 500)
-		}
-		return
-	}
-	http.FileServer(http.Dir(base)).ServeHTTP(w, r)
-}
-
-func isDoc(path string) bool {
-	_, ok := contentTemplate[filepath.Ext(path)]
-	return ok
-}
-
-func initTemplates(base string) error {
-	// Locate the template file.
-	actionTmpl := filepath.Join(base, "templates/action.tmpl")
-
-	contentTemplate = make(map[string]*template.Template)
-
-	for ext, contentTmpl := range map[string]string{
-		".slide": "slides.tmpl",
-	} {
-		contentTmpl = filepath.Join(base, "templates", contentTmpl)
-
-		// Read and parse the input.
-		tmpl := present.Template()
-		tmpl = tmpl.Funcs(template.FuncMap{"playable": playable})
-		if _, err := tmpl.ParseFiles(actionTmpl, contentTmpl); err != nil {
-			return err
-		}
-		contentTemplate[ext] = tmpl
-	}
-	return nil
-}
-
-// renderDoc reads the present file, gets its template representation,
-// and executes the template, sending output to w.
-func renderDoc(w io.Writer, docFile string) error {
-	// Read the input and build the doc structure.
-	doc, err := parse(docFile, 0)
-	if err != nil {
-		return err
-	}
-	// Find which template should be executed.
-	tmpl := contentTemplate[filepath.Ext(docFile)]
-	// Execute the template.
-	return doc.Render(w, tmpl)
-}
-
-func parse(name string, mode present.ParseMode) (*present.Doc, error) {
-	f, err := os.Open(name)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-	return present.Parse(f, name, 0)
-}
-
-var modTime = time.Now()
-var scriptByte []byte
-
-func playHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-type", "application/javascript")
-	http.ServeContent(w, r, "", modTime, bytes.NewReader(scriptByte))
-}
-
-func playScript(root, transport string) {
-	var buf bytes.Buffer
-	for _, p := range scripts {
-		if s, ok := static.Files[p]; ok {
-			buf.WriteString(s)
-			continue
-		}
-		b, err := ioutil.ReadFile(filepath.Join(root, "./static", p))
-		if err != nil {
-			panic(err)
-		}
-		buf.Write(b)
-	}
-	fmt.Fprintf(&buf, "\ninitPlayground(new %v());\n", transport)
-	scriptByte = buf.Bytes()
 }
